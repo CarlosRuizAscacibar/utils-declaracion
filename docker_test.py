19952f8aac3f2accb683e46e34933f916eae87c9
Carlos Ruiz <carlos.ruiz.ascacibar@gmail.com>
2025-09-07 20:18:28 +0200

    prepare for docker

---
+++ docker_test.py
@@ -0,0 +1,3 @@
+import os
+var_value = os.getenv("JSON_BANK_MY_INVESTOR")
+print(var_value)
---
+++ launch_homeserver.sh
@@ -0,0 +1,3 @@
+#docker --context homeserver compose --env-file .env.homeserver run --rm -it utils_declaration --build
+#docker --context homeserver compose --env-file .env.homeserver build
+docker --context homeserver compose --env-file .env.homeserver run --rm -it utils_declaration
