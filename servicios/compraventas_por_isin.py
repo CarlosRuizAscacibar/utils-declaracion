faea8d26104666f12ab803afb2103756a43f52fe
Carlos Ruiz <carlos.ruiz.ascacibar@gmail.com>
2025-10-18 20:14:29 +0200

    añadido splits a compraventa por isin

--- servicios/compraventas_por_isin.py
+++ servicios/compraventas_por_isin.py
@@ -1,8 +1,9 @@
 from modelos.operacion import Operacion
+from modelos.split import Split
 from modelos.tipo_operacion import TipoOperacion
 from modelos.compra_venta import CompraVenta
 
-def operaciones_por_isin(operaciones: list[Operacion]) -> list[CompraVenta]:
+def operaciones_por_isin(operaciones: list[Operacion | Split]) -> list[CompraVenta]:
     op_isin_dic: dict[str, list[Operacion]] = agrupar_por_isin(operaciones=operaciones)
     compra_ventas : list[CompraVenta] = []
     for isin in op_isin_dic:
@@ -11,7 +12,7 @@ def operaciones_por_isin(operaciones: list[Operacion]) -> list[CompraVenta]:
     compra_ventas = sorted(compra_ventas, key=lambda x: x.venta.fecha.isoformat())
     return compra_ventas
     
-def agrupar_por_isin(operaciones: list[Operacion]) -> dict[str, list[Operacion]]:
+def agrupar_por_isin(operaciones: list[Operacion | Split]) -> dict[str, list[Operacion]]:
     dic_isin = dict()
     for op in operaciones:
         if op.isin not in dic_isin:
@@ -19,26 +20,37 @@ def agrupar_por_isin(operaciones: list[Operacion]) -> dict[str, list[Operacion]]
         dic_isin[op.isin].append(op)
     
     for clave in dic_isin:
-        dic_isin[clave] = sorted(dic_isin[clave], key= lambda x: x.fecha.isoformat() + x.tipo.name)
+        dic_isin[clave] = sorted(dic_isin[clave], key= sort_key)
 
     return dic_isin
 
-def compra_ventas_por_isin(operaciones: list[Operacion]) -> list[CompraVenta]:
+def sort_key(x: Split | Operacion):
+    key = x.fecha.isoformat()
+    if hasattr(x, "tipo"):
+        key = key + x.tipo.name
+    return key
+
+def compra_ventas_por_isin(operaciones: list[Operacion | Split]) -> list[CompraVenta]:
     compra_ventas: list[CompraVenta] = []
     compras_con_acciones: list[Operacion] = []
     for x in operaciones:
-        if x.tipo == TipoOperacion.COMPRA:
-            compras_con_acciones.append(x)
-        if x.tipo == TipoOperacion.VENTA:
-            while len(compras_con_acciones) > 0 and x.restantes > 0:
-                cantidad = x.restantes
-                if cantidad > compras_con_acciones[0].restantes:
-                    cantidad = compras_con_acciones[0].restantes
-                compra_ventas.append(CompraVenta(compra=compras_con_acciones[0], venta=x, cantidad=cantidad))
-                compras_con_acciones[0].restantes = compras_con_acciones[0].restantes - cantidad
-                if compras_con_acciones[0].restantes == 0:
-                    compras_con_acciones.pop(0)
-                x.restantes = x.restantes - cantidad
-            if len(compras_con_acciones) == 0 and x.restantes > 0:
-                raise Exception(f"Accion sin compras {x.isin} {x.fecha} {x.tipo.name} {x.cantidad} {x.restantes}")
+        if isinstance(x, Split):
+            for y in compras_con_acciones:
+                y.restantes = y.restantes / x.numOriginal
+                y.restantes = y.restantes * x.numDestino
+        else:
+            if x.tipo == TipoOperacion.COMPRA:
+                compras_con_acciones.append(x)
+            if x.tipo == TipoOperacion.VENTA:
+                while len(compras_con_acciones) > 0 and x.restantes > 0:
+                    cantidad = x.restantes
+                    if cantidad > compras_con_acciones[0].restantes:
+                        cantidad = compras_con_acciones[0].restantes
+                    compra_ventas.append(CompraVenta(compra=compras_con_acciones[0], venta=x, cantidad=cantidad))
+                    compras_con_acciones[0].restantes = compras_con_acciones[0].restantes - cantidad
+                    if compras_con_acciones[0].restantes == 0:
+                        compras_con_acciones.pop(0)
+                    x.restantes = x.restantes - cantidad
+                if len(compras_con_acciones) == 0 and x.restantes > 0:
+                    raise Exception(f"Accion sin compras {x.isin} {x.fecha} {x.tipo.name} {x.cantidad} {x.restantes}")
     return compra_ventas
--- tests/servicios/test_compraventas_por_isin.py
+++ tests/servicios/test_compraventas_por_isin.py
@@ -1,3 +1,4 @@
+from modelos.split import Split
 from servicios.compraventas_por_isin import operaciones_por_isin, agrupar_por_isin
 from modelos.tipo_operacion import TipoOperacion
 from datetime import datetime
@@ -43,3 +44,36 @@ class TestCompraVentasPorIsin(unittest.TestCase):
         self.assertEqual(dic_isin[1].cantidad,3)
         self.assertEqual(dic_isin[1].compra,op2)
         self.assertEqual(dic_isin[1].venta,op3)
+    
+    def test_operaciones_por_isin_varias_compras_una_ventas_y_sobran(self):
+        op1 = Operacion(fecha=datetime.strptime('01-01-2020', '%d-%m-%Y'),isin='ISIN1',cantidad=2,precio_unitario=1,tipo=TipoOperacion.COMPRA, restantes=2)
+        op2 = Operacion(fecha=datetime.strptime('03-01-2020', '%d-%m-%Y'),isin='ISIN1',cantidad=4,precio_unitario=3,tipo=TipoOperacion.COMPRA, restantes=4)
+        op3 = Operacion(fecha=datetime.strptime('04-01-2020', '%d-%m-%Y'),isin='ISIN1',cantidad=5,precio_unitario=2,tipo=TipoOperacion.VENTA, restantes=5)
+        operaciones = [op1,op2,op3]
+
+        dic_isin = operaciones_por_isin(operaciones)
+        self.assertEqual(dic_isin[0].cantidad,2)
+        self.assertEqual(dic_isin[0].compra,op1)
+        self.assertEqual(dic_isin[0].venta,op3)
+        self.assertEqual(dic_isin[1].cantidad,3)
+        self.assertEqual(dic_isin[1].compra,op2)
+        self.assertEqual(dic_isin[1].venta,op3)
+        self.assertEqual(op1.restantes, 0)
+        self.assertEqual(op2.restantes, 1)
+
+    def test_operaciones_por_isin_varias_compras_una_ventas_y_split(self):
+        op1 = Operacion(fecha=datetime.strptime('01-01-2020', '%d-%m-%Y'),isin='ISIN1',cantidad=2,precio_unitario=1,tipo=TipoOperacion.COMPRA, restantes=2)
+        op2 = Operacion(fecha=datetime.strptime('03-01-2020', '%d-%m-%Y'),isin='ISIN1',cantidad=4,precio_unitario=3,tipo=TipoOperacion.COMPRA, restantes=4)
+        op3 = Operacion(fecha=datetime.strptime('04-01-2020', '%d-%m-%Y'),isin='ISIN1',cantidad=5,precio_unitario=2,tipo=TipoOperacion.VENTA, restantes=5)
+        op4 = Split(id='split_isin1', isin='ISIN1', fecha=datetime.strptime('05-01-2020', '%d-%m-%Y'), numOriginal=1, numDestino=10)
+        operaciones = [op1,op2,op3, op4]
+
+        dic_isin = operaciones_por_isin(operaciones)
+        self.assertEqual(dic_isin[0].cantidad,2)
+        self.assertEqual(dic_isin[0].compra,op1)
+        self.assertEqual(dic_isin[0].venta,op3)
+        self.assertEqual(dic_isin[1].cantidad,3)
+        self.assertEqual(dic_isin[1].compra,op2)
+        self.assertEqual(dic_isin[1].venta,op3)
+        self.assertEqual(op1.restantes, 0)
+        self.assertEqual(op2.restantes, 10)
